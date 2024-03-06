james5294/openpilot

Compile FrogPilot

selfdrive/locationd/models/generated/car.h
@@ -1,0 +1,40 @@
+#pragma once
+#include "rednose/helpers/ekf.h"
+extern "C" {
+void car_update_25(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_24(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_30(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_26(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_27(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_29(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_28(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_31(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_err_fun(double *nom_x, double *delta_x, double *out_6290441625309936060);
+void car_inv_err_fun(double *nom_x, double *true_x, double *out_5314558907334496336);
+void car_H_mod_fun(double *state, double *out_7895949841719061513);
+void car_f_fun(double *state, double dt, double *out_4613932111658201338);
+void car_F_fun(double *state, double dt, double *out_7388683364835999016);
+void car_h_25(double *state, double *unused, double *out_3327535608170938215);
+void car_H_25(double *state, double *unused, double *out_2233137525527874346);
+void car_h_24(double *state, double *unused, double *out_3234738581906136368);
+void car_H_24(double *state, double *unused, double *out_5185254108149052547);
+void car_h_30(double *state, double *unused, double *out_3758132126270666246);
+void car_H_30(double *state, double *unused, double *out_6760833855655482544);
+void car_h_26(double *state, double *unused, double *out_3891213047040668096);
+void car_H_26(double *state, double *unused, double *out_5974640844401930570);
+void car_h_27(double *state, double *unused, double *out_1294650421285170099);
+void car_H_27(double *state, double *unused, double *out_8935597167455907455);
+void car_h_29(double *state, double *unused, double *out_2590388325194524509);
+void car_H_29(double *state, double *unused, double *out_6250602511341090360);
+void car_h_28(double *state, double *unused, double *out_7541101478745343567);
+void car_H_28(double *state, double *unused, double *out_7113742545298930682);
+void car_h_31(double *state, double *unused, double *out_6964985136424960565);
+void car_H_31(double *state, double *unused, double *out_6600848946635282046);
+void car_predict(double *in_x, double *in_P, double *in_Q, double dt);
+void car_set_mass(double x);
+void car_set_rotational_inertia(double x);
+void car_set_center_to_front(double x);
+void car_set_center_to_rear(double x);
+void car_set_stiffness_front(double x);
+void car_set_stiffness_rear(double x);
+}

selfdrive/locationd/models/generated/live.h
@@ -1,0 +1,38 @@
+#pragma once
+#include "rednose/helpers/ekf.h"
+extern "C" {
+void live_update_4(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_9(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_10(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_12(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_35(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_32(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_13(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_14(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_33(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_H(double *in_vec, double *out_7867863003839610759);
+void live_err_fun(double *nom_x, double *delta_x, double *out_6614048919106816820);
+void live_inv_err_fun(double *nom_x, double *true_x, double *out_2996437008730679516);
+void live_H_mod_fun(double *state, double *out_5938837053370187441);
+void live_f_fun(double *state, double dt, double *out_5943072507876609054);
+void live_F_fun(double *state, double dt, double *out_3595316619751676824);
+void live_h_4(double *state, double *unused, double *out_8504012056138441135);
+void live_H_4(double *state, double *unused, double *out_6815516860047217919);
+void live_h_9(double *state, double *unused, double *out_5642394112647587368);
+void live_H_9(double *state, double *unused, double *out_471702075217229551);
+void live_h_10(double *state, double *unused, double *out_5651455266028130303);
+void live_H_10(double *state, double *unused, double *out_6972082645737282945);
+void live_h_12(double *state, double *unused, double *out_3489187020732200148);
+void live_H_12(double *state, double *unused, double *out_5249968836619600701);
+void live_h_35(double *state, double *unused, double *out_6804786205546796194);
+void live_H_35(double *state, double *unused, double *out_3448854802674610543);
+void live_h_32(double *state, double *unused, double *out_1638716263979791125);
+void live_H_32(double *state, double *unused, double *out_3317690017733870074);
+void live_h_13(double *state, double *unused, double *out_5873540218692768727);
+void live_H_13(double *state, double *unused, double *out_8265398910829609374);
+void live_h_14(double *state, double *unused, double *out_5642394112647587368);
+void live_H_14(double *state, double *unused, double *out_471702075217229551);
+void live_h_33(double *state, double *unused, double *out_4706085852221705334);
+void live_H_33(double *state, double *unused, double *out_6747731490599103886);
+void live_predict(double *in_x, double *in_P, double *in_Q, double dt);
+}

abraxas4/FrogPilot_forked

selfdrive/locationd/models/generated/car.h
@@ -1,0 +1,40 @@
+#pragma once
+#include "rednose/helpers/ekf.h"
+extern "C" {
+void car_update_25(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_24(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_30(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_26(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_27(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_29(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_28(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_update_31(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void car_err_fun(double *nom_x, double *delta_x, double *out_2724251513109582597);
+void car_inv_err_fun(double *nom_x, double *true_x, double *out_8376452329691755560);
+void car_H_mod_fun(double *state, double *out_5184655659434066940);
+void car_f_fun(double *state, double dt, double *out_2131619432792421671);
+void car_F_fun(double *state, double dt, double *out_600621081321688166);
+void car_h_25(double *state, double *unused, double *out_7067729207585171189);
+void car_H_25(double *state, double *unused, double *out_2870604109442777267);
+void car_h_24(double *state, double *unused, double *out_3796831930133441469);
+void car_H_24(double *state, double *unused, double *out_6428529708245100403);
+void car_h_30(double *state, double *unused, double *out_479901281108876026);
+void car_H_30(double *state, double *unused, double *out_1657092220684830931);
+void car_h_26(double *state, double *unused, double *out_6522861422684189753);
+void car_H_26(double *state, double *unused, double *out_870899209431278957);
+void car_h_27(double *state, double *unused, double *out_1424999336504214354);
+void car_H_27(double *state, double *unused, double *out_566501850499112286);
+void car_h_29(double *state, double *unused, double *out_5766549776351297442);
+void car_H_29(double *state, double *unused, double *out_1146860876370438747);
+void car_h_28(double *state, double *unused, double *out_7407923175810356107);
+void car_H_28(double *state, double *unused, double *out_6229259893439969321);
+void car_h_31(double *state, double *unused, double *out_8382621941290469267);
+void car_H_31(double *state, double *unused, double *out_1497107311664630433);
+void car_predict(double *in_x, double *in_P, double *in_Q, double dt);
+void car_set_mass(double x);
+void car_set_rotational_inertia(double x);
+void car_set_center_to_front(double x);
+void car_set_center_to_rear(double x);
+void car_set_stiffness_front(double x);
+void car_set_stiffness_rear(double x);
+}

selfdrive/locationd/models/generated/live.h
@@ -1,0 +1,38 @@
+#pragma once
+#include "rednose/helpers/ekf.h"
+extern "C" {
+void live_update_4(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_9(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_10(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_12(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_35(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_32(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_13(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_14(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_update_33(double *in_x, double *in_P, double *in_z, double *in_R, double *in_ea);
+void live_H(double *in_vec, double *out_4746088412224613480);
+void live_err_fun(double *nom_x, double *delta_x, double *out_9001890752258480632);
+void live_inv_err_fun(double *nom_x, double *true_x, double *out_7751484030935475721);
+void live_H_mod_fun(double *state, double *out_6372284872494722072);
+void live_f_fun(double *state, double dt, double *out_7737125286734520429);
+void live_F_fun(double *state, double dt, double *out_6924291271128232703);
+void live_h_4(double *state, double *unused, double *out_7163581597376063005);
+void live_H_4(double *state, double *unused, double *out_1282798183191685382);
+void live_h_9(double *state, double *unused, double *out_1398902136694561631);
+void live_H_9(double *state, double *unused, double *out_4171659735471764724);
+void live_h_10(double *state, double *unused, double *out_1154166129467252208);
+void live_H_10(double *state, double *unused, double *out_5904459669970046663);
+void live_h_12(double *state, double *unused, double *out_5174139242233862891);
+void live_H_12(double *state, double *unused, double *out_8949926496874135874);
+void live_h_35(double *state, double *unused, double *out_8755816883392181434);
+void live_H_35(double *state, double *unused, double *out_4649460240564292758);
+void live_h_32(double *state, double *unused, double *out_6465677489100694951);
+void live_H_32(double *state, double *unused, double *out_949950172302789475);
+void live_h_13(double *state, double *unused, double *out_8354810049473215286);
+void live_H_13(double *state, double *unused, double *out_347859678244556079);
+void live_h_14(double *state, double *unused, double *out_1398902136694561631);
+void live_H_14(double *state, double *unused, double *out_4171659735471764724);
+void live_h_33(double *state, double *unused, double *out_3730663583502984644);
+void live_H_33(double *state, double *unused, double *out_7800017245203150362);
+void live_predict(double *in_x, double *in_P, double *in_Q, double dt);
+}